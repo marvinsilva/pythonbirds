573a8c963d782ff4ab7e73a49801e868915832ae
marvinsilva <vnc_vinicius92@hotmail.com>
2020-02-18 20:49:54 -0300

    Criado métodos de classe e decorators

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     marcus = Pessoa(nome='Marcus')
@@ -34,3 +41,6 @@ if __name__ == '__main__':
     print(marcus.olhos)
     print(luciano.olhos)
     print(id(Pessoa.olhos), id(marcus.olhos), id(luciano.olhos))
+
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
