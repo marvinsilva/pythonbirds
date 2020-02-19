6991fbc81adae3d01019a5ff3e838c3ba08f6744
marvinsilva <vnc_vinicius92@hotmail.com>
2020-02-19 09:59:00 -0300

    Criado Doctest para carro e suas classes

--- oo/carro.py
+++ oo/carro.py
@@ -1,5 +1,96 @@
+"""
+Você deve criar uma classe carro que vai possuir
+dois atributos compostos por outras duas classes:
+1) Motor
+2) Direção
+O Motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dado velocidade
+2) Método acelerar, que deverá incremetar a velocidade de uma unidade
+3) Método frear que deverá decrementar a velocidade em duas unidades
+A Direção terá a responsabilidade de controlar a direção. Ela oferece
+os seguintes atributos:
+1) Valor de diração com valores possíveis: Norte, Sul, Leste, Oeste.
+2) Método girar_a_direita
+2) Método girar_a_esquerda
+  N
+O   L
+  S
+Exemplo:
+    >>> # Testando motor
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    1
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    2
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    3
+    >>> motor.frear()
+    >>> motor.velocidade
+    1
+    >>> motor.frear()
+    >>> motor.velocidade
+    0
+    >>> # Testando Direcao
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Norte'
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Oeste'
+"""
+
 class Carro:
-    def __init__(self, motor, direcao):
+    def __init__(self, direcao, motor):
         self.direcao = direcao
         self.motor = motor
 
@@ -57,77 +148,3 @@ class Direcao:
             self.valor = 'Leste'
         elif self.valor == 'Leste':
             self.valor = 'Norte'
-
-if __name__ == '__main__':
-
-    motor = Motor()
-    print(motor.velocidade)
-
-    motor.acelerar()
-    print(motor.velocidade)
-
-    motor.acelerar()
-    print(motor.velocidade)
-
-    motor.acelerar()
-    print(motor.velocidade)
-
-    motor.frear()
-    print(motor.velocidade)
-
-    motor.frear()
-    print(motor.velocidade)
-
-    print()
-
-    direcao = Direcao()
-    print(direcao.valor)
-
-    direcao.girar_a_direita()
-    print(direcao.valor)
-
-    direcao.girar_a_direita()
-    print(direcao.valor)
-
-    direcao.girar_a_direita()
-    print(direcao.valor)
-
-    direcao.girar_a_direita()
-    print(direcao.valor)
-
-    direcao.girar_a_esquerda()
-    print(direcao.valor)
-
-    direcao.girar_a_esquerda()
-    print(direcao.valor)
-
-    direcao.girar_a_esquerda()
-    print(direcao.valor)
-
-    direcao.girar_a_esquerda()
-    print(direcao.valor)
-
-    print()
-
-    carro = Carro(motor, direcao)
-    print(carro.calcular_velocidade())
-
-    carro.acelerar()
-    print(carro.calcular_velocidade())
-
-    carro.acelerar()
-    print(carro.calcular_velocidade())
-
-    carro.frear()
-    print(carro.calcular_velocidade())
-
-    print(carro.calcular_direcao())
-
-    carro.girar_a_direita()
-    print(carro.calcular_direcao())
-
-    carro.girar_a_esquerda()
-    print(carro.calcular_direcao())
-
-    carro.girar_a_esquerda()
-    print(carro.calcular_direcao())
